68d7fd39905369175f6df1797c6450583ccc2fd5
Arthur Rocha Laszlo Liszkievich <arthur.liszkievich@gmail.com>
2025-03-17 10:19:46 -0300

    Criação o model  "Contact"

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+# Blank "campo opcional"
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
