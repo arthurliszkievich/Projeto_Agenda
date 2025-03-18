48b8acdbe02dd4218d8d00aff6d1e19d4649010a
Arthur Rocha Laszlo Liszkievich <arthur.liszkievich@gmail.com>
2025-03-18 15:32:40 -0300

    FK Owner & Category

--- contact/admin.py
+++ contact/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from contact.models import Contact
+from contact.models import Category
 
 # Register your models here.
 
@@ -13,3 +14,9 @@ class ContactAdmin(admin.ModelAdmin):
     list_per_page = 10
     list_max_show_all = 50
     list_editable = ("phone",)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    ordering = ("id",)
+   
--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Blank "campo opcional (somente em formulários Django)"
+
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+    
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Contact(models.Model):
@@ -13,6 +26,12 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(upload_to="pictures/%Y/%m/", blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, blank=True, null=True
+    )
+    owner = models.ForeignKey (
+        User, on_delete=models.SET_NULL, blank=True, null=True
+    )
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
